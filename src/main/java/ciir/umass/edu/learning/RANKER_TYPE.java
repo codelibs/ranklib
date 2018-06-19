5f1bf478e050939cd1be1ca37f2d377fc99b0d35
Shinsuke Sugaya <shinsuke@apache.org>
2018-06-19 12:50:10 +0900

    add RANKER_TYPE

---
+++ src/main/java/ciir/umass/edu/learning/RANKER_TYPE.java
@@ -0,0 +1,21 @@
+package ciir.umass.edu.learning;
+
+/**
+ * Use RankerType.
+ *
+ * @author shinsuke
+ *
+ */
+@Deprecated
+public class RANKER_TYPE {
+    public static final RankerType MART = RankerType.MART;
+    public static final RankerType RANKBOOST = RankerType.RANKBOOST;
+    public static final RankerType RANKNET = RankerType.RANKNET;
+    public static final RankerType ADARANK = RankerType.ADARANK;
+    public static final RankerType COOR_ASCENT = RankerType.COOR_ASCENT;
+    public static final RankerType LAMBDARANK = RankerType.LAMBDARANK;
+    public static final RankerType LAMBDAMART = RankerType.LAMBDAMART;
+    public static final RankerType LISTNET = RankerType.LISTNET;
+    public static final RankerType RANDOM_FOREST = RankerType.RANDOM_FOREST;
+    public static final RankerType LINEAR_REGRESSION = RankerType.LINEAR_REGRESSION;
+}
